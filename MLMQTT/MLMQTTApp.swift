9a0513c4051b8eb93a598c0243d09768797cf077
AdnannM <adnannmuratovic@icloud.com>
2025-05-29 15:26:20 +0200

    Initial Commit

---
+++ MLMQTT/MLMQTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLMQTTApp.swift
+//  MLMQTT
+//
+//  Created by Adnann Muratovic on 29.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MLMQTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
